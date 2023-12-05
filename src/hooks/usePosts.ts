9874af87ff8d0139a43181fb74eea377207a0666
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-12-05 10:57:03 +0530

    Update usePosts hook to useInfiniteQuery for pagination

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 interface Post {
   id: number;
@@ -8,27 +8,63 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
-const usePosts = (query: PostQuery) => {
-  const fetchPosts = () =>
-    axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
-        params: {
-          _start: (query.page - 1) * query.pageSize,
-          _limit: query.pageSize,
-        },
-      })
-      .then((res) => res.data);
-
-  return useQuery({
+const usePosts = (query: PostQuery) =>
+  useInfiniteQuery({
     queryKey: ["posts", query],
-    queryFn: fetchPosts,
+    queryFn: ({ pageParam = 1 }) =>
+      axios
+        .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (pageParam - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
+        .then((res) => res.data),
     staleTime: 10 * 1000,
     placeholderData: keepPreviousData,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
+    initialPageParam: 1,
   });
-};
 
 export default usePosts;
+
+// ----------------------Pagination-------------------------
+// import { keepPreviousData, useQuery } from "@tanstack/react-query";
+// import axios from "axios";
+// interface Post {
+//   id: number;
+//   title: string;
+//   body: string;
+//   userId: number;
+// }
+
+// interface PostQuery {
+//   page: number;
+//   pageSize: number;
+// }
+
+// const usePosts = (query: PostQuery) => {
+//   const fetchPosts = () =>
+//     axios
+//       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+//         params: {
+//           _start: (query.page - 1) * query.pageSize,
+//           _limit: query.pageSize,
+//         },
+//       })
+//       .then((res) => res.data);
+
+//   return useQuery({
+//     queryKey: ["posts", query],
+//     queryFn: fetchPosts,
+//     staleTime: 10 * 1000,
+//     placeholderData: keepPreviousData,
+//   });
+// };
+
+// export default usePosts;
