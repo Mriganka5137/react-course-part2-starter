39e5491acc2e6a28474c13877e2c29f30b963958
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-12-05 09:51:37 +0530

    Pagination in React Query

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,34 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) => {
+  const fetchPosts = () =>
+    axios
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize,
+        },
+      })
+      .then((res) => res.data);
+
+  return useQuery({
+    queryKey: ["posts", query],
+    queryFn: fetchPosts,
+    staleTime: 10 * 1000,
+    placeholderData: keepPreviousData,
+  });
+};
+
+export default usePosts;
