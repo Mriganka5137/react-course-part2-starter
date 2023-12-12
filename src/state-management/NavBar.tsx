a607b348721075c3e8a892102f984d32911e8791
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-12-12 11:42:41 +0530

    Refactor NavBar component and counter store
    This commit refactors the NavBar component by removing unused imports and updating the counter state access. It also updates the counter store by adding a new 'max' property and updating the reset function.

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,11 +1,9 @@
-import { useContext } from "react";
 import LoginStatus from "./auth/LoginStatus";
-import TasksContexts from "./tasks/tasksContext";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-  const { counter } = useCounterStore();
-  const { tasks } = useContext(TasksContexts);
+  const counter = useCounterStore((s) => s.counter);
+  console.log("Render");
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>
